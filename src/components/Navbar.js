ee5ff875b6c89d72a4b1227498cdb2fd9150be74
Akshay Khurana <Akshaykhurana9002@gmail.com>
2022-06-29 16:26:23 +0530

    Added Navbar and Movies Card

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="nav">
+        <div className="search-container">
+            <input/>
+            <button id = "search-btn">search
+            </button>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
